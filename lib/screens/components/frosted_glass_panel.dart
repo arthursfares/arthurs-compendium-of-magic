5612f90bf3287ed193dee7a4d72840de4da9ea2d
arthursfares <arthursfares@gmail.com>
2022-04-07 00:32:00 -0300

    :art: Create FrostedGlassPanel component

---
+++ lib/screens/components/frosted_glass_panel.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class FrostedGlassPanel extends StatelessWidget {
+  const FrostedGlassPanel({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Center(
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+          child: Container(
+            width: size.width * 0.9,
+            height: size.height * 0.60,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(child: child),
+          ),
+        ),
+      ),
+    );
+  }
+}
