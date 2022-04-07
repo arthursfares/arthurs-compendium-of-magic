e8e9ea86b884c0a970066a4dcfbca0a2772c03a2
arthursfares <arthursfares@gmail.com>
2022-04-07 03:24:03 -0300

    :lipstick: Add fab circular menu and remove AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,33 @@
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HELLO THERE.'),
+      floatingActionButton: FabCircularMenu(
+        fabOpenIcon: const FaIcon(FontAwesomeIcons.skull),
+        fabCloseIcon: const FaIcon(FontAwesomeIcons.xmark),
+        ringDiameter: size.width,
+        // ringColor: const Color(0x00ffffff),
+        ringColor: const Color(0x11161616),
+        children: <Widget>[
+          IconButton(
+              icon: const FaIcon(FontAwesomeIcons.mugHot), onPressed: () {}),
+          IconButton(
+              icon: const FaIcon(FontAwesomeIcons.wandMagicSparkles), onPressed: () {}),
+          IconButton(
+              icon: const FaIcon(FontAwesomeIcons.bookSkull), onPressed: () {}),
+          IconButton(
+              icon: const FaIcon(FontAwesomeIcons.hatWizard), onPressed: () {}),
+        ],
+        animationDuration: const Duration(milliseconds: 600),
+        animationCurve: Curves.linearToEaseOut,
       ),
     );
   }
