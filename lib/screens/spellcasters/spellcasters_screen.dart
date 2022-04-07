47c39c258fed6ddd25fff5538fa8f17f8bf108b2
arthursfares <arthursfares@gmail.com>
2022-04-07 17:30:21 -0300

    :recycle: Refactor spellcasters screen to use FabMenu

--- lib/screens/spellcasters/spellcasters_screen.dart
+++ lib/screens/spellcasters/spellcasters_screen.dart
@@ -1,42 +1,13 @@
-import 'package:fab_circular_menu/fab_circular_menu.dart';
+import 'package:arthurs_compendium_of_magic/screens/components/fab_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SpellcastersScreen extends StatelessWidget {
   const SpellcastersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    return Scaffold(
-      floatingActionButton: FabCircularMenu(
-        fabOpenIcon: const FaIcon(FontAwesomeIcons.skull),
-        fabCloseIcon: const FaIcon(FontAwesomeIcons.xmark),
-        ringDiameter: size.width,
-        // ringColor: const Color(0x00ffffff),
-        ringColor: const Color(0x11161616),
-        children: <Widget>[
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.mugHot),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.wandMagicSparkles),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.bookSkull),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.hatWizard),
-            onPressed: () {},
-          ),
-        ],
-        animationDuration: const Duration(milliseconds: 600),
-        animationCurve: Curves.linearToEaseOut,
-      ),
+    return const Scaffold(
+      floatingActionButton: FabMenu(),
     );
   }
 }
