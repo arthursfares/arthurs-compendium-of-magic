045e2247b0e5e6a0c7153bb8a6b8a477595df458
arthursfares <arthursfares@gmail.com>
2022-04-06 22:36:48 -0300

    :art: Create RoundedGradientButton component

---
+++ lib/screens/components/rounded_gradient_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class RoundedGradientButton extends StatelessWidget {
+  const RoundedGradientButton({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      margin: const EdgeInsets.all(10),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, 'home');
+        },
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.zero),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(80.0),
+            ),
+          ),
+        ),
+        child: Ink(
+          decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Color(0xff9254C8), Color(0xffE15FED)],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+              borderRadius: BorderRadius.circular(30.0)),
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
+            alignment: Alignment.center,
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white, fontSize: 15),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
