88a5c7c8a55e616fd865cb0ac5686325432a30a4
arthursfares <arthursfares@gmail.com>
2022-04-08 12:10:00 -0300

    :lipstick: Change ui for adding new spellcasters
    Spelcasters screen now uses AppBar's action button to add new charater
    instead of body's ElevatedButton.

--- lib/screens/spellcasters/components/spellcasters_body.dart
+++ lib/screens/spellcasters/components/spellcasters_body.dart
@@ -1,83 +1,15 @@
 import 'package:arthurs_compendium_of_magic/screens/spellcasters/components/spellcaster_card.dart';
 import 'package:flutter/material.dart';
 
-class SpellcastersBody extends StatefulWidget {
-  const SpellcastersBody({Key? key}) : super(key: key);
+class SpellcastersBody extends StatelessWidget {
+  const SpellcastersBody({
+    Key? key,
+    required this.names,
+    required this.images,
+  }) : super(key: key);
 
-  @override
-  State<SpellcastersBody> createState() => _SpellcastersBodyState();
-}
-
-class _SpellcastersBodyState extends State<SpellcastersBody> {
-  final List<String> names = <String>[
-    'Larry Bird',
-    'Victor',
-    'Tim',
-  ];
-  final List<String> images = <String>[
-    'assets/images/larry-bird.png',
-    'assets/images/victor.png',
-    'assets/images/tim.png',
-  ];
-
-  TextEditingController nameController = TextEditingController();
-
-  void addItemToList() {
-    setState(() {
-      // names.insert(0, nameController.text);
-      names.insert(names.length, nameController.text);
-      images.insert(images.length, 'assets/images/mordenkainen.png');
-    });
-  }
-
-  Future<void> _displayTextInputDialog(BuildContext context) async {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Name'),
-            content: TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                  hintText: "TODO: random mage name generator"),
-            ),
-            actions: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    setState(() {
-                      Navigator.pop(context);
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red[800], // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton(
-                  child: const Text('ADD'),
-                  onPressed: () {
-                    setState(() {
-                      addItemToList();
-                      nameController.clear();
-                      Navigator.pop(context);
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green[700], // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                ),
-              ),
-            ],
-          );
-        });
-  }
+  final List<String> names;
+  final List<String> images;
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +17,7 @@ class _SpellcastersBodyState extends State<SpellcastersBody> {
 
     return SizedBox(
       width: size.width,
-      height: size.height * 0.84,
+      height: size.height,
       child: Column(
         children: <Widget>[
           Flexible(
@@ -101,27 +33,6 @@ class _SpellcastersBodyState extends State<SpellcastersBody> {
                   imagePath: images[index],
                 );
               },
-            ),
-          ),
-          const SizedBox(height: 13.0),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 13.0),
-                child: Text(
-                  '📚 Add spellcaster',
-                  style: TextStyle(fontSize: 18.0),
-                ),
-              ),
-              onPressed: () {
-                // addItemToList();
-                _displayTextInputDialog(context);
-              },
-              style: ElevatedButton.styleFrom(
-                primary: const Color.fromARGB(255, 146, 109, 213), // background
-                onPrimary: Colors.white, // foreground
-              ),
             ),
           ),
         ],
--- lib/screens/spellcasters/spellcasters_screen.dart
+++ lib/screens/spellcasters/spellcasters_screen.dart
@@ -1,17 +1,105 @@
 import 'package:arthurs_compendium_of_magic/screens/components/fab_menu.dart';
 import 'package:arthurs_compendium_of_magic/screens/spellcasters/components/spellcasters_body.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SpellcastersScreen extends StatelessWidget {
+class SpellcastersScreen extends StatefulWidget {
   const SpellcastersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SpellcastersScreen> createState() => _SpellcastersScreenState();
+}
+
+class _SpellcastersScreenState extends State<SpellcastersScreen> {
+  final List<String> names = <String>[
+    'Larry Bird',
+    'Victor',
+    'Tim',
+  ];
+
+  final List<String> images = <String>[
+    'assets/images/larry-bird.png',
+    'assets/images/victor.png',
+    'assets/images/tim.png',
+  ];
+
+  TextEditingController nameController = TextEditingController();
+
+  void addItemToList() {
+    setState(() {
+      // names.insert(0, nameController.text);
+      names.insert(names.length, nameController.text);
+      images.insert(images.length, 'assets/images/mordenkainen.png');
+    });
+  }
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Name'),
+            content: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                  hintText: "TODO: random mage name generator"),
+            ),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    setState(() {
+                      Navigator.pop(context);
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red[800], // background
+                    onPrimary: Colors.white, // foreground
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  child: const Text('ADD'),
+                  onPressed: () {
+                    setState(() {
+                      addItemToList();
+                      nameController.clear();
+                      Navigator.pop(context);
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green[700], // background
+                    onPrimary: Colors.white, // foreground
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Spellcasters'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: IconButton(
+              icon: const FaIcon(FontAwesomeIcons.plus),
+              onPressed: (){
+                _displayTextInputDialog(context);
+              },
+            ),
+          ),
+        ],
       ),
-      body: const SpellcastersBody(),
+      body: SpellcastersBody(names: names, images: images),
       floatingActionButton: const FabMenu(),
     );
   }
