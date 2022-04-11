fb12498e2d8b9f01bae06c6c0b2ff5a21e017222
arthursfares <arthursfares@gmail.com>
2022-04-11 00:42:13 -0300

    :sparkles: Add D&D 5e spell list to AddSpellBody
    Clicking on a spell does not save it to spellbook, only pops context.
    Also, still lacks filtering.

--- lib/screens/add-spell/components/add_spell_body.dart
+++ lib/screens/add-spell/components/add_spell_body.dart
@@ -1,12 +1,63 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class AddSpellBody extends StatelessWidget {
-  const AddSpellBody({ Key? key }) : super(key: key);
+import 'package:arthurs_compendium_of_magic/models/spell_token.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class AddSpellBody extends StatefulWidget {
+  const AddSpellBody({Key? key}) : super(key: key);
+
+  @override
+  State<AddSpellBody> createState() => _AddSpellBodyState();
+}
+
+class _AddSpellBodyState extends State<AddSpellBody> {
+  Client client = Client();
+  List<SpellToken> spellList = [];
+
+  loadTokensJson() async {
+    final response =
+        await client.get(Uri.parse('https://www.dnd5eapi.co/api/spells/'));
+    if (response.statusCode == 200) {
+      final dynamic decodedResponse = await json.decode(response.body);
+      setState(() {
+        decodedResponse['results'].forEach((item) {
+          spellList.add(SpellToken.fromJson(item));
+        });
+      });
+    } else {
+      throw Exception('Failed to load tokens');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) async {
+      await loadTokensJson();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemCount: spellList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text(spellList[index].name),
+              onTap: () async {
+                // TODO: save selected token in data file to be accessed by 
+                // spellbook body.
+                Navigator.pop(context);
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
