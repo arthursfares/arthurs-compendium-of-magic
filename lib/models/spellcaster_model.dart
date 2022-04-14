5ef8e2ed39e06c8d40f6d5ac1a841657ac148fc9
arthursfares <arthursfares@gmail.com>
2022-04-14 05:53:07 -0300

    :fire: Remove classes map from SpellcasterModel

--- lib/models/spellcaster_model.dart
+++ lib/models/spellcaster_model.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 
 class SpellcasterModel {
   late String _spellcasterName;
-  late Map<String, int> _spellcasterClassMap;
-  late Image _spellcasterThumbnail;
+  // late Map<String, int> _spellcasterClassMap;
+  late AssetImage _spellcasterThumbnail;
 
   SpellcasterModel(
     spellcasterName,
-    spellcasterClassMap,
+    // spellcasterClassMap,
     spellcasterThumbnail,
-  );
+  ) {
+    _spellcasterName = spellcasterName;
+    _spellcasterThumbnail = spellcasterThumbnail;
+  }
 
   String get spellcasterName => _spellcasterName;
-  Map<String, int> get spellcasterClassMap => _spellcasterClassMap;
-  Image get spellcasterThumbnail => _spellcasterThumbnail;
+  // Map<String, int> get spellcasterClassMap => _spellcasterClassMap;
+  AssetImage get spellcasterThumbnail => _spellcasterThumbnail;
 }
