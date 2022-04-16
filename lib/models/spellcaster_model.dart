18d88535ea7a4a1fa6b76067e5b5e28cf492ef58
arthursfares <arthursfares@gmail.com>
2022-04-16 07:10:02 -0300

    :sparkles: Add description, class and level parameters to SpellcasterModel

--- lib/models/spellcaster_model.dart
+++ lib/models/spellcaster_model.dart
@@ -3,18 +3,27 @@ import 'package:flutter/material.dart';
 class SpellcasterModel {
   late String _name;
   late AssetImage _thumbnail;
-  // late Map<String, int> _spellcasterClassMap;
+  late String _dndClass;
+  late int _level;
+  late String _description;
 
   SpellcasterModel(
-    spellcasterName,
-    spellcasterThumbnail,
-    // spellcasterClassMap,
+    String spellcasterName,
+    AssetImage spellcasterThumbnail,
+    String spellcasterClass,
+    int spellcasterLevel,
+    String spellcasterDescription,
   ) {
     _name = spellcasterName;
     _thumbnail = spellcasterThumbnail;
+    _dndClass = spellcasterClass;
+    _level = spellcasterLevel;
+    _description = spellcasterDescription;
   }
 
   String get name => _name;
   AssetImage get thumbnail => _thumbnail;
-  // Map<String, int> get spellcasterClassMap => _spellcasterClassMap;
+  String      get dndClass      => _dndClass;
+  int         get level         => _level;
+  String      get description   => _description;
 }
