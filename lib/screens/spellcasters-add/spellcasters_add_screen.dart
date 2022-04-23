04546e99504ec4b0c6dd06bbd2d29543f9255bd8
arthursfares <arthursfares@gmail.com>
2022-04-23 20:13:47 -0300

    :adhesive_bandage: Fix image picker and add image cropper

--- lib/screens/spellcasters-add/spellcasters_add_screen.dart
+++ lib/screens/spellcasters-add/spellcasters_add_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:numberpicker/numberpicker.dart';
 
@@ -14,8 +15,31 @@ class SpellcastersAddScreen extends StatefulWidget {
 
 class _SpellcastersAddScreenState extends State<SpellcastersAddScreen> {
   final textFieldController = TextEditingController();
-  PickedFile _imageFile = PickedFile("assets/images/ken-thompson.png");
-  final ImagePicker _picker = ImagePicker();
+  File? image = File("assets/images/euler.png");
+  // PickedFile _imageFile = PickedFile("assets/images/euler.png");
+  // final ImagePicker _picker = ImagePicker();
+
+  /// Open image gallery and pick an image
+  Future<XFile?> pickImageFromGallery() async {
+    return await ImagePicker().pickImage(source: ImageSource.gallery);
+  }
+
+  /// Pick image from gallery and return a file
+  Future<File?> cropSelectedImage(String filePath) async {
+    ImageCropper imageCropper = ImageCropper();
+
+    return await imageCropper.cropImage(
+      sourcePath: filePath,
+      aspectRatio: const CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
+      iosUiSettings: const IOSUiSettings(
+        title: 'Crop Image',
+        aspectRatioLockEnabled: true,
+        minimumAspectRatio: 1.0,
+        aspectRatioPickerButtonHidden: true,
+      ),
+      // androidUiSettings: AndroidUiSettings(),
+    );
+  }
 
   @override
   void dispose() {
@@ -28,17 +52,29 @@ class _SpellcastersAddScreenState extends State<SpellcastersAddScreen> {
       child: Stack(children: <Widget>[
         CircleAvatar(
           radius: 80.0,
-          backgroundImage: FileImage(File(_imageFile.path)),
+          backgroundImage: FileImage(image!),
         ),
         Positioned(
           bottom: 20.0,
           right: 20.0,
           child: InkWell(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                builder: ((builder) => bottomSheet()),
-              );
+            onTap: () async {
+              // Step #1: Pick Image From Gallery.
+              await pickImageFromGallery().then((pickedFile) async {
+                // Step #2: Check if we actually picked an image. Otherwise -> stop;
+                if (pickedFile == null) return;
+
+                // Step #3: Crop earlier selected image
+                await cropSelectedImage(pickedFile.path).then((croppedFile) {
+                  // Step #4: Check if we actually cropped an image. Otherwise -> stop;
+                  if (croppedFile == null) return;
+
+                  // Step #5: Display image on screen
+                  setState(() {
+                    image = croppedFile;
+                  });
+                });
+              });
             },
             child: const Icon(
               Icons.camera_alt,
@@ -51,52 +87,52 @@ class _SpellcastersAddScreenState extends State<SpellcastersAddScreen> {
     );
   }
 
-  Widget bottomSheet() {
-    return Container(
-      height: 100.0,
-      width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 20,
-      ),
-      child: Column(
-        children: <Widget>[
-          const Text(
-            "Choose Profile photo",
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            TextButton.icon(
-              icon: const Icon(Icons.camera),
-              onPressed: () {
-                takePhoto(ImageSource.camera);
-              },
-              label: const Text("Camera"),
-            ),
-            TextButton.icon(
-              icon: const Icon(Icons.image),
-              onPressed: () {
-                takePhoto(ImageSource.gallery);
-              },
-              label: const Text("Gallery"),
-            ),
-          ])
-        ],
-      ),
-    );
-  }
+  // Widget bottomSheet() {
+  //   return Container(
+  //     height: 100.0,
+  //     width: MediaQuery.of(context).size.width,
+  //     margin: const EdgeInsets.symmetric(
+  //       horizontal: 20,
+  //       vertical: 20,
+  //     ),
+  //     child: Column(
+  //       children: <Widget>[
+  //         const Text(
+  //           "Choose Profile photo",
+  //           style: TextStyle(
+  //             fontSize: 20.0,
+  //           ),
+  //         ),
+  //         const SizedBox(
+  //           height: 20,
+  //         ),
+  //         Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+  //           TextButton.icon(
+  //             icon: const Icon(Icons.camera),
+  //             onPressed: () {
+  //               takePhoto(ImageSource.camera);
+  //             },
+  //             label: const Text("Camera"),
+  //           ),
+  //           TextButton.icon(
+  //             icon: const Icon(Icons.image),
+  //             onPressed: () {
+  //               takePhoto(ImageSource.gallery);
+  //             },
+  //             label: const Text("Gallery"),
+  //           ),
+  //         ])
+  //       ],
+  //     ),
+  //   );
+  // }
 
-  void takePhoto(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-    setState(() {
-      _imageFile = pickedFile as PickedFile;
-    });
-  }
+  // void takePhoto(ImageSource source) async {
+  //   final pickedFile = await _picker.pickImage(source: source);
+  //   setState(() {
+  //     _imageFile = pickedFile as PickedFile;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -187,6 +223,13 @@ class _SpellcastersAddScreenState extends State<SpellcastersAddScreen> {
         tooltip: 'Done',
         child: const Icon(CommunityMaterialIcons.brain),
         onPressed: () {
+          // TODO: add new spellcaster to list
+          // for now it only adds the default one
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.grey[200],
+            content: const Text('Spellcaster added successfully :)'),
+            duration: const Duration(seconds: 3),
+          ));
           Navigator.pop(context, textFieldController.text);
         },
       ),
