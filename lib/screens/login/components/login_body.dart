6556f9f82bc780bd56592ab9f27b82b3cf1ce0c8
arthursfares <arthursfares@gmail.com>
2022-04-26 03:35:26 -0300

    :lipstick: Replace password field with input field

--- lib/screens/login/components/login_body.dart
+++ lib/screens/login/components/login_body.dart
@@ -3,45 +3,56 @@ import 'package:arthurs_compendium_of_magic/screens/components/app_logo.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/forgot_password_check.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/frosted_glass_panel.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/outline_border_input_field.dart';
-import 'package:arthurs_compendium_of_magic/screens/components/outline_border_password_field.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/rounded_gradient_button.dart';
 import 'package:arthurs_compendium_of_magic/screens/login/components/login_background.dart';
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginBody extends StatelessWidget {
   const LoginBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return LoginBackground(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const AppLogo(),
-          FrostedGlassPanel(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const OutlineBorderInputField(
-                    labelText: 'Username', icon: Icons.person),
-                const OutlineBorderPasswordField(
-                    labelText: 'Password', icon: Icons.lock),
-                ForgotPasswordCheck(press: () {
-                  // print('forgot password');
-                }),
-                RoundedGradientButton(
-                  text: 'Log in',
-                  press: () {
-                    Navigator.pushReplacementNamed(context, 'spellcasters');
-                  },
+          SizedBox(height: size.height / 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: FrostedGlassPanel(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20.0),
+                    const OutlineBorderInputField(
+                        labelText: 'E-mail', icon: FontAwesomeIcons.solidChessRook),
+                    const OutlineBorderInputField(
+                        labelText: 'Password', icon: CommunityMaterialIcons.key, obscureText: true),
+                    ForgotPasswordCheck(press: () {
+                      // print('forgot password');
+                    }),
+                    RoundedGradientButton(
+                      text: 'Log in',
+                      press: () {
+                        Navigator.pushReplacementNamed(context, 'spellcasters');
+                      },
+                    ),
+                    AlreadyHaveAnAccountCheck(
+                      login: true,
+                      press: () {
+                        Navigator.pushReplacementNamed(context, 'signup');
+                      },
+                    ),
+                    const SizedBox(height: 30.0),
+                  ],
                 ),
-                AlreadyHaveAnAccountCheck(
-                  login: true,
-                  press: () {
-                    Navigator.pushReplacementNamed(context, 'signup');
-                  },
-                ),
-              ],
+              ),
             ),
           ),
         ],
--- lib/screens/signup/components/signup_body.dart
+++ lib/screens/signup/components/signup_body.dart
@@ -2,42 +2,53 @@ import 'package:arthurs_compendium_of_magic/screens/components/already_have_an_a
 import 'package:arthurs_compendium_of_magic/screens/components/app_logo.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/frosted_glass_panel.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/outline_border_input_field.dart';
-import 'package:arthurs_compendium_of_magic/screens/components/outline_border_password_field.dart';
 import 'package:arthurs_compendium_of_magic/screens/components/rounded_gradient_button.dart';
 import 'package:arthurs_compendium_of_magic/screens/signup/components/signup_background.dart';
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignupBody extends StatelessWidget {
   const SignupBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return SignupBackground(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const AppLogo(),
-          FrostedGlassPanel(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const OutlineBorderInputField(
-                    labelText: 'Username', icon: Icons.person),
-                const OutlineBorderInputField(
-                    labelText: 'E-mail', icon: Icons.mail),
-                const OutlineBorderPasswordField(
-                    labelText: 'Password', icon: Icons.lock),
-                RoundedGradientButton(
-                  text: 'Sign up',
-                  press: () {},
+          SizedBox(height: size.height / 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: FrostedGlassPanel(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20.0),
+                    const OutlineBorderInputField(
+                        labelText: 'Username', icon: FontAwesomeIcons.cat),
+                    const OutlineBorderInputField(
+                        labelText: 'E-mail', icon: FontAwesomeIcons.solidChessRook),
+                    const OutlineBorderInputField(
+                        labelText: 'Password', icon: CommunityMaterialIcons.key, obscureText: true),
+                    RoundedGradientButton(
+                      text: 'Sign up',
+                      press: () {},
+                    ),
+                    AlreadyHaveAnAccountCheck(
+                      login: false,
+                      press: () {
+                        Navigator.pushReplacementNamed(context, 'login');
+                      },
+                    ),
+                    const SizedBox(height: 30.0),
+                  ],
                 ),
-                AlreadyHaveAnAccountCheck(
-                  login: false,
-                  press: () {
-                    Navigator.pushReplacementNamed(context, 'login');
-                  },
-                ),
-              ],
+              ),
             ),
           ),
         ],
