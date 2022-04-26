36f91f9471ff1a9f36654804f8e717632a71a948
arthursfares <arthursfares@gmail.com>
2022-04-26 03:31:40 -0300

    :sparkles: Update input field parameters and wrap it with Focus widget

--- lib/screens/components/outline_border_input_field.dart
+++ lib/screens/components/outline_border_input_field.dart
@@ -1,33 +1,69 @@
 import 'package:flutter/material.dart';
 
-class OutlineBorderInputField extends StatelessWidget {
+class OutlineBorderInputField extends StatefulWidget {
   const OutlineBorderInputField({
     Key? key,
     required this.labelText,
     required this.icon,
+    this.controller,
+    this.onChanged,
+    this.obscureText = false,
   }) : super(key: key);
 
   final String labelText;
   final IconData icon;
+  final TextEditingController? controller;
+  final Function(String)? onChanged;
+  final bool? obscureText;
+
+  @override
+  State<OutlineBorderInputField> createState() =>
+      _OutlineBorderInputFieldState();
+}
+
+class _OutlineBorderInputFieldState extends State<OutlineBorderInputField> {
+  Color _colorText = Colors.white70;
 
   @override
   Widget build(BuildContext context) {
+    const _defaultColor = Colors.white70;
+    const _focusColor = Color.fromARGB(212, 146, 84, 200);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      child: Theme(
-        data: ThemeData(
-          colorScheme: ThemeData().colorScheme.copyWith(
-                primary: const Color.fromARGB(212, 146, 84, 200),
-              ),
-        ),
+      child: Focus(
+        onFocusChange: (hasFocus) {
+          // When you focus on input email, you need to notify the color change into the widget.
+          setState(() => _colorText = hasFocus ? _focusColor : _defaultColor);
+        },
         child: TextField(
-          obscureText: false,
+          controller: widget.controller,
+          onChanged: widget.onChanged,
+          obscureText: widget.obscureText!,
+          cursorColor: _colorText,
           decoration: InputDecoration(
             border: const OutlineInputBorder(),
-            labelText: labelText,
             prefixIcon: Icon(
-              icon,
-              color: Colors.blueGrey,
+              widget.icon,
+              color: _colorText,
+            ),
+            labelText: widget.labelText,
+            labelStyle: const TextStyle(color: Colors.white70),
+            floatingLabelStyle: TextStyle(
+              color: _colorText,
+              fontWeight: FontWeight.bold,
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white70,
+                width: 1.2,
+              ),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color.fromARGB(212, 146, 84, 200),
+                width: 2.5,
+              ),
             ),
           ),
         ),
