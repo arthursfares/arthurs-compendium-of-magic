e039c856fa46e95d8ad2bf3e7aa175031e22b2ff
arthursfares <arthursfares@gmail.com>
2022-04-30 18:15:16 -0300

    :sparkles: Create SpellcasterListModel (ChangeNotifier)

---
+++ lib/models/spellcaster_list_model.dart
@@ -0,0 +1,26 @@
+import 'package:arthurs_compendium_of_magic/models/spellcaster_model.dart';
+import 'package:flutter/widgets.dart';
+
+class SpellcasterListModel extends ChangeNotifier {
+  final List<SpellcasterModel> _spellcasters = <SpellcasterModel>[
+    SpellcasterModel(
+      'Nick Cage',
+      Image.asset('assets/images/nic-cage.png'),
+      'Wizard',
+      19,
+      'I\'m going to steal the Declaration of Independence',
+    ),
+  ];
+
+  List<SpellcasterModel> get spellcasters => _spellcasters;
+
+  void add(SpellcasterModel spellcaster) {
+    _spellcasters.add(spellcaster);
+    notifyListeners();
+  }
+
+  void remove(int index) {
+    _spellcasters.removeAt(index);
+    notifyListeners();
+  }
+}
