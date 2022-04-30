ab9dd26d1d6d3deea0e51594a0a49dc6cda98a40
arthursfares <arthursfares@gmail.com>
2022-04-30 18:21:25 -0300

    :sparkles: Update to use Consumer

--- lib/screens/spellcasters/components/card_options_dropdown.dart
+++ lib/screens/spellcasters/components/card_options_dropdown.dart
@@ -1,8 +1,18 @@
+import 'dart:ui';
+
+import 'package:arthurs_compendium_of_magic/models/spellcaster_list_model.dart';
 import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class CardOptionsDropdown extends StatelessWidget {
-  const CardOptionsDropdown({Key? key}) : super(key: key);
+  const CardOptionsDropdown({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
+
+  final int index;
 
   @override
   Widget build(BuildContext context) {
@@ -35,16 +45,31 @@ class CardOptionsDropdown extends StatelessWidget {
         } else if (value == 1) {
           showDialog<String>(
             context: context,
-            builder: (BuildContext context) => AlertDialog(
-              backgroundColor: const Color(0xFF161616),
-              title: const Text('Confirmation'),
-              content: const Text(
-                  'Your about to delete a beloved character with unique magical knowlege and personal histories. \n\nAre you shure you want to continue?'),
-              actions: <Widget>[
-                TextButton(
-                  child: Text(
-                    'DELETE',
-                    style: TextStyle(color: Colors.red.shade800),
+            builder: (BuildContext context) => BackdropFilter(
+              blendMode: BlendMode.src,
+              filter: ImageFilter.blur(
+                sigmaX: 8,
+                sigmaY: 8,
+              ),
+              child: AlertDialog(
+                backgroundColor: const Color(0xFF161616),
+                title: const Text('Confirmation'),
+                content: const Text(
+                    'Your about to delete a beloved character with unique magical knowlege and personal histories. \n\nAre you shure you want to continue?'),
+                actions: <Widget>[
+                  Consumer<SpellcasterListModel>(
+                    builder: (context, spellcasters, child) {
+                      return TextButton(
+                        child: Text(
+                          'DELETE',
+                          style: TextStyle(color: Colors.red.shade800),
+                        ),
+                        onPressed: () {
+                          spellcasters.remove(index);
+                          Navigator.pop(context);
+                        },
+                      );
+                    },
                   ),
                   onPressed: () {
                     Navigator.pop(context);
