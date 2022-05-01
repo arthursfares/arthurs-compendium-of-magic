311cca6f1f6c1314f940701bef6cf2cff9aa98a2
arthursfares <arthursfares@gmail.com>
2022-05-01 16:07:22 -0300

    :sparkles: Update about screen

--- lib/screens/about/components/about_body.dart
+++ lib/screens/about/components/about_body.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutBody extends StatelessWidget {
   const AboutBody({Key? key}) : super(key: key);
@@ -9,18 +11,52 @@ class AboutBody extends StatelessWidget {
 
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          CircleAvatar(
+            radius: 100,
+            backgroundImage:
+                Image.asset('assets/images/bermuda-mage.png').image,
+          ),
+          const SizedBox(height: 15.0),
+          Text.rich(
+            TextSpan(
+              style: const TextStyle(
+                fontSize: 27,
+              ),
+              children: [
+                TextSpan(
+                  style: TextStyle(
+                    color: Colors.purple.shade400,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  text: "github.com/arthursfares",
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () async {
+                      String url = "https://github.com/arthursfares";
+                      var urlLaunchable = await canLaunchUrl(Uri.parse(url));
+                      if (urlLaunchable) {
+                        await launchUrl(Uri.parse(url));
+                      }
+                    },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Container(height: 2, width: size.width / 1.2, color: Colors.grey.shade800),
+          const SizedBox(height: 20.0),
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Text(
-              'App for storing Dungeons & Dragons characters\' spell list, in order to make them easier to consult during the game.',
+              'App for storing Dungeons & Dragons characters\' spell list, in order to make them easier to consult during the game.\n\nDeveloped for Flutter class.',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 16),
             ),
           ),
           const SizedBox(height: 20.0),
-          Container(height: 2, width:  size.width / 1.2, color: Colors.black12),
+          Container(height: 2, width: size.width / 1.2, color: Colors.grey.shade800),
           const SizedBox(height: 20.0),
           const Text(
             'by',
@@ -37,12 +73,21 @@ class AboutBody extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20.0),
-          Container(height: 2, width:  size.width / 1.2, color: Colors.black12),
+          Container(height: 2, width: size.width / 1.2, color: Colors.grey.shade800),
           const SizedBox(height: 20.0),
           const Text(
             'Made with 💙 and ☕.',
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 80.0),
+          Text(
+            'Powered by D&D 5e API',
+            style: TextStyle(
+              color: Colors.grey.shade200,
+              fontStyle: FontStyle.italic,
+              fontSize: 16,
+            ),
           ),
         ],
       ),
