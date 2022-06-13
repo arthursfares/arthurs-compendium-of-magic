58491be920ed4586e0819832f9c26a677648ccd4
arthursfares <arthursfares@gmail.com>
2022-06-13 05:59:05 -0300

    :sparkles: Add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:arthurs_compendium_of_magic/firebase_options.dart';
 import 'package:arthurs_compendium_of_magic/models/spellcaster_list_model.dart';
 import 'package:arthurs_compendium_of_magic/screens/about/about_screen.dart';
 import 'package:arthurs_compendium_of_magic/screens/settings/settings_screen.dart';
@@ -7,28 +8,32 @@ import 'package:arthurs_compendium_of_magic/screens/spellcasters-add/spellcaster
 import 'package:arthurs_compendium_of_magic/screens/spellcasters/spellcasters_screen.dart';
 import 'package:arthurs_compendium_of_magic/screens/login/login_screen.dart';
 import 'package:arthurs_compendium_of_magic/screens/signup/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => SpellcasterListModel(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: const Color(0xFF191919)),
-        initialRoute: 'login',
-        routes: {
-          'login':              (context) => const LoginScreen(),
-          'signup':             (context) => const SignupScreen(),
-          'spellcasters':       (context) => const SpellcastersScreen(),
-          'spellcasters-add':   (context) => const SpellcastersAddScreen(),
-          'spellbook':          (context) => const SpellbookScreen(),
-          'spellbook-add':      (context) => const SpellbookAddScreen(),
-          'about':              (context) => const AboutScreen(),
-          'settings':           (context) => const SettingsScreen(),
-        },
-      ),
-    )
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(ChangeNotifierProvider(
+    create: (context) => SpellcasterListModel(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: const Color(0xFF191919)),
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => const LoginScreen(),
+        'signup': (context) => const SignupScreen(),
+        'spellcasters': (context) => const SpellcastersScreen(),
+        'spellcasters-add': (context) => const SpellcastersAddScreen(),
+        'spellbook': (context) => const SpellbookScreen(),
+        'spellbook-add': (context) => const SpellbookAddScreen(),
+        'about': (context) => const AboutScreen(),
+        'settings': (context) => const SettingsScreen(),
+      },
+    ),
+  ));
 }
