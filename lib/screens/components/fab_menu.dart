0cb25e2f38bd354c0311f597640ab463976f2493
arthursfares <arthursfares@gmail.com>
2022-06-13 05:48:03 -0300

    :fire: Remove unused imports

--- lib/screens/components/fab_menu.dart
+++ lib/screens/components/fab_menu.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
